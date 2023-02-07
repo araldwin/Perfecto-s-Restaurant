8b15579e925a37e89c6c032cf7731749c52a9985
araldwin <araldwin@gmail.com>
2023-02-07 13:18:15 +0000

    create new project

--- book/models.py
+++ book/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
-# Create your models here.
+now = timezone.now()
+
+
+class Book(models.Model):
+    name = models.CharField('Your Name', max_length=50)
+    email = models.EmailField('Your Email', max_length=50)
+    phone = models.CharField('Your Phone', max_length=50)
+    date = models.DateField(default=datetime.now)
+    time = models.TimeField(default=datetime.now)
+    people = models.CharField('# of People', max_length=3)
+    message = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
