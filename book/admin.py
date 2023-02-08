7baa9242edfb8481073d84c526dffc089e466be1
araldwin <araldwin@gmail.com>
2023-02-08 11:32:51 +0000

    create admin manage site, create book,branch, myrestaurantuser apps at models.py

--- book/admin.py
+++ book/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from .models import Branch
+from .models import MyRestaurantUser
 from .models import Book
 
 
+admin.site.register(Branch)
+admin.site.register(MyRestaurantUser)
 admin.site.register(Book)
--- book/models.py
+++ book/models.py
@@ -5,14 +5,37 @@ from django.utils import timezone
 now = timezone.now()
 
 
+class Branch(models.Model):
+    name = models.CharField('Branch Name', max_length=120)
+    address = models.CharField(max_length=300)
+    web = models.URLField('Website Address')
+    branch_email = models.EmailField('Branch Email', max_length=50)
+    branch_phone = models.CharField('Branch Phone', max_length=50)
+
+    # allows to pop up on the page and it will list the name
+    def __str__(self):
+        return self.name
+
+
+class MyRestaurantUser(models.Model):
+    first_name = models.CharField('First Name', max_length=50)
+    last_name = models.CharField('Last Name', max_length=50)
+    email = models.EmailField('Email', max_length=50)
+
+    # allows to pop up on the page and it will list the name
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
 class Book(models.Model):
     name = models.CharField('Your Name', max_length=50)
-    email = models.EmailField('Your Email', max_length=50)
     phone = models.CharField('Your Phone', max_length=50)
-    date = models.DateField(default=datetime.now)
-    time = models.TimeField(default=datetime.now)
+    branch = models.ForeignKey(Branch, blank=True, null=True, on_delete=models.CASCADE)
+    book_date = models.DateTimeField(default=datetime.now)
     people = models.CharField('# of People', max_length=3)
     message = models.TextField(blank=True)
+    bookings = models.ManyToManyField(MyRestaurantUser, blank=True)
 
+    # allows to pop up on the page and it will list the name
     def __str__(self):
         return self.name
