b1dba1ea0285f4211a2b60fde22c8f5d29c61def
araldwin <araldwin@gmail.com>
2023-02-22 09:58:22 +0000

    create account.html, include account to base, mofidy style into navbar and add about, add style for navbar nad login container.

--- book/forms.py
+++ book/forms.py
@@ -5,12 +5,17 @@ from .models import Book
 
 # Create a Booking form
 class BookForm(ModelForm):
-    book_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control', 'placeholder': 'Date'}))
-    book_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time', 'step': 60, 'class': 'form-control', 'placeholder': 'Time'}))
+    book_date = forms.DateField(widget=forms.DateInput(
+        attrs={'type': 'date', 'class': 'form-control', 'placeholder': 'Date'})
+        )
+    book_time = forms.TimeField(widget=forms.TimeInput(
+        attrs={'type': 'time', 'step': 60, 'class': 'form-control',
+                                                    'placeholder': 'Time'}))
 
     class Meta:
         model = Book
-        fields = ('name', 'phone', 'branch', 'book_date', 'book_time', 'people', 'message')
+        fields = ('name', 'phone', 'branch', 'book_date',
+                  'book_time', 'people', 'message')
 
         labels = {
             'name': 'Name',
@@ -23,9 +28,15 @@ class BookForm(ModelForm):
 
         }
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your full name'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Phone'}),
-            'branch': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Which branch'}),
-            'people': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '# of people'}),
-            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}),
+            'name': forms.TextInput(attrs={'class': 'form-control',
+                                    'placeholder': 'Your full name'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control',
+                                     'placeholder': 'Your Phone'}),
+            'branch': forms.Select(attrs={'class': 'form-control',
+                                          'placeholder': 'Which branch'}),
+            'people': forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': '# of people'}),
+            'message': forms.Textarea(attrs={'class': 'form-control',
+                                      'placeholder': 'Your Message'}),
+                                      
         }
