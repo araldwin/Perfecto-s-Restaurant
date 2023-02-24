04192f64a36261c1b66e0b9baf42e6b586d4d828
araldwin <araldwin@gmail.com>
2023-02-24 12:00:33 +0000

    major change to models.py remove myrestaurant user class

--- book/models.py
+++ book/models.py
@@ -15,16 +15,6 @@ by the Django project settings (usually the timezone of the server running the D
 '''
 
 
-class MyRestaurantUser(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    first_name = models.CharField('First Name', max_length=50)
-    last_name = models.CharField('Last Name', max_length=50)
-    email = models.EmailField('Email', blank=True, max_length=50)
-
-    # allows to pop up on the page and it will list the name
-    def __str__(self):
-        return self.first_name + ' ' + self.last_name
-
 
 class Book(models.Model):
     '''
@@ -35,14 +25,15 @@ class Book(models.Model):
     which allows Book objects to be represented as 
     strings in a human-readable format.
     '''
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField('Guest Name', max_length=50)
     phone = models.CharField('Guest Phone', max_length=50)
+    email = models.EmailField('Email', blank=True, max_length=50)
     book_date = models.DateField(default=datetime.now)
     book_time = models.TimeField(default=datetime.now)
     people = models.CharField('# of Guest', max_length=2)
     message = models.TextField(blank=True)
-    myrestaurantuser = models.ForeignKey(MyRestaurantUser, blank=True,
-                                         null=True, on_delete=models.CASCADE)
+    
 
     # allows to pop up on the page and it will list the name
     def __str__(self):
--- book/views.py
+++ book/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Book, MyRestaurantUser
+from .models import Book 
 from .forms import BookForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
