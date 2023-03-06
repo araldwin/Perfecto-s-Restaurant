e597f2a1b549fcd5b38cc8cfd9ff7d83ce8e44df
araldwin <araldwin@gmail.com>
2023-03-06 12:31:58 +0000

    deployment test

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['myrestaurant2023.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -148,4 +148,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
