b7d4b41bb3de5d32405d1124b6f957cae0092cf3
araldwin <araldwin@gmail.com>
2023-03-07 06:48:14 +0000

    add style to 404 page

--- food/views.py
+++ food/views.py
@@ -7,6 +7,7 @@ def food_list(request):
     context = {'food_list': food_list}
     return render(request, 'food_list.html', context)
 
+
 def food_detail(request, slug):
     food = Food.objects.get(slug=slug)
     context = {'food': food}
